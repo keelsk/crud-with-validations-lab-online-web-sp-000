05a41157230ca0e5bf16850c8a3f51f1015d49fe
Kirsten Keels <keelsk@gmail.com>
2020-04-17 01:37:20 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,12 +3,13 @@ class Song < ApplicationRecord
   validates :title, uniqueness: true
   validates :released, inclusion: {in: [true, false]}
   validates :release_year, presence:true, if: :released_exists?
+  validates :release_year, presence:true, if: :released_exists?
   validates :release_year, numericality: {less_than_or_equal_to: Time.new.year}
   validates :artist_name, presence: true
 
   private
   def released_exists?
-    self.released
+    self.released==true
   end
 
   def release_year_check
