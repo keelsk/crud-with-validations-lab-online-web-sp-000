4f857e6501f3fd33ce5efb7083f32a3f8472221b
Kirsten Keels <keelsk@gmail.com>
2020-04-17 01:49:22 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,14 +2,14 @@ class Song < ApplicationRecord
   validates :title, presence: true
   validates :title, uniqueness: true
   validates :released, inclusion: {in: [true, false]}
-  validates :release_year, presence:true, if: :released_exists?
+  validates :release_year, presence:true, unless: :released_exist?
   validates :release_year, presence: false, allow_nil: true, if: !:released_exists?
   validates :release_year, numericality: {less_than_or_equal_to: Time.new.year}
   validates :artist_name, presence: true
 
   private
   def released_exists?
-    self.released==true
+    self.released==false
   end
 
   # def once_a_year
